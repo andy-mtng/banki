f0b95eb80ef9fbe0617ad87e51db6e94bdd46402
Andy N <andynguyen9001@gmail.com>
2023-05-24 21:18:11 -0600

    Add navigation for routes depending on authentication

--- client/src/RouteSwitch.js
+++ client/src/RouteSwitch.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App";
 import CategoriesContainer from "./CategoriesContainer";
 import LandingPage from "./LandingPage";
 import Login from "./Login";
 import Signup from "./Signup";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 const RouteSwitch = () => {
+    const { user } = useAuthContext();
+
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LandingPage />}/>
-                <Route path="/categories" element={<CategoriesContainer />} />
-                <Route path="/categories/:category" element={<App />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
+                <Route path="/" element={!user ? <LandingPage /> : <Navigate to="/categories" /> }/>
+                <Route path="/categories" element={user ? <CategoriesContainer /> : <Navigate to="/login" />} />
+                <Route path="/categories/:category" element={user ? <App /> : <Navigate to="/login" />} />
+                <Route path="/login" element={user ? <App /> : <Login />} />
+                <Route path="/signup" element={user ? <App /> : <Signup />} />
             </Routes>
         </BrowserRouter>
     )
